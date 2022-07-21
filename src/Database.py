1f55d86bce8415ba86cc2523cb0f732ca4cad9b5
Hritwik <hritwiks.int@media.net>
2022-07-21 17:01:23 +0530

    refacter

--- src/Database.py
+++ src/Database.py
@@ -1,18 +1,22 @@
 import json
+from pathlib import Path
 
 
 class Database:
 
     def __init__(self):
         self.db_location = "my_db.db"
-        self.db: dict = json.loads(self.db_location)
+        with open(self.db_location, 'w+') as my_db:
+            my_db.write("{}")
+            self.db: dict = json.load(my_db)
 
     def __int__(self, location: str):
         self.db_location = location
-        self.db: dict = json.loads(self.db_location)
+        with open(self.db_location) as my_db:
+            self.db: dict = json.load(my_db)
 
     def write_db_to_file(self) -> None:
-        with open(self.db_location) as my_file:
+        with open(self.db_location, 'r+') as my_file:
             json.dump(self.db, my_file)
 
     def read_file(self, location) -> dict:
@@ -26,7 +30,7 @@ class Database:
         print("get", key)
         print(self.db[key])
 
-    def put(self, pair, db: dict):
+    def put(self, pair: str):
         print("PUT ", pair)
         self.db = self.db | json.loads(pair)
         self.write_db_to_file()
@@ -35,7 +39,7 @@ class Database:
         self.db = db | self.read_file(file_path)
         self.write_db_to_file()
 
-    def delete(self, key: str, db: dict):
+    def delete(self, key: str):
         print("delete ", key)
         self.db.pop(key)
         self.write_db_to_file()
