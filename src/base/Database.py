a387b78f1af1f7e5be0217fbfd23878b80597984
Hritwik <hritwiks.int@media.net>
2022-07-22 16:18:57 +0530

    add truncate DB option

--- src/base/Database.py
+++ src/base/Database.py
@@ -8,11 +8,7 @@ class Database:
         """default constructor, creates an empty DB and loads it into memory"""
         self.db_location = "my_db.db"
         if not os.path.isfile(self.db_location):
-            with open(self.db_location, 'w+') as my_db:
-                # some key and value are needed for json to read the file
-                json.dump({"": ""}, my_db, indent=4)
-                my_db.seek(0)
-                self.db: dict = json.load(my_db)
+            self._reset_db()
         else:
             with open(self.db_location) as my_db:
                 self.db: dict = json.load(my_db)
@@ -24,16 +20,27 @@ class Database:
         with open(self.db_location) as my_db:
             self.db: dict = json.load(my_db)
 
-    def write_db_to_file(self) -> None:
+    def _reset_db(self):
+        with open(self.db_location, 'w+') as my_db:
+            # some key and value are needed for json to read the file
+            json.dump({"": ""}, my_db, indent=4)
+            my_db.seek(0)
+            self.db: dict = json.load(my_db)
+
+    def _write_db_to_file(self) -> None:
         """write json data to file"""
-        # Very Poorly optimized
+
+        # check lock, if its there then wait
+        ### code
+        # create lock
         with open(self.db_location, 'w') as my_file:
             json.dump(self.db, my_file, indent=4)
 
             # If we don't seek to start, the json won't be able to read since after end, it's all empty
             my_file.seek(0, 0)
+        # remove lock
 
-    def read_file(self, location) -> dict:
+    def _read_file(self, location) -> dict:
         """load json data from file"""
         with open(location) as my_file:
             data: dict = json.load(my_file)
@@ -53,7 +60,7 @@ class Database:
         try:
             key, value = pair.split("=")
             self.db = self.db | json.loads(f'{{"{key}": "{value}"}}')
-            self.write_db_to_file()
+            self._write_db_to_file()
             return f"PUT:: Successfully put {key}={value} in Database"
         except:
             return f"PUT:: Some error occurred while putting {pair} in DB"
@@ -62,8 +69,8 @@ class Database:
     def put_from_file(self, file_path: str) -> str:
         """put values from json-fied file to DB"""
 
-        self.db = self.db | self.read_file(file_path)
-        self.write_db_to_file()
+        self.db = self.db | self._read_file(file_path)
+        self._write_db_to_file()
         return f"Successfully Added entries from {file_path} in DB"
 
     def delete(self, key: str) -> str:
@@ -71,7 +78,7 @@ class Database:
 
         try:
             self.db.pop(key)
-            self.write_db_to_file()
+            self._write_db_to_file()
             return f"DELETE:: Successfully deleted {key} from DB"
         except KeyError:
             return f"DELETE:: Key {key} does not exist in DB"
@@ -79,3 +86,7 @@ class Database:
     def show(self) -> dict:
         """returns the database as dict"""
         return self.db
+
+    def truncate(self) -> None:
+        """truncate current DB"""
+        self._reset_db()
